78195013712c78dcfb3a888403dc8b24beabf0e9
AnesVijay <2011andreyka@gmail.com>
2021-11-23 21:02:15 +0500

    Исправил грубые ошибки в old_filter.py

--- Krayzel Andrey/old_filter.py
+++ Krayzel Andrey/old_filter.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
-img = Image.open("scale1200.jpg")
-arr = np.array(img)
+
+img = Image.open("CP77-KV-en.jpg")
+arr = np.array(img, dtype=np.int32)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
@@ -10,19 +11,19 @@ while i < a - 11:
     while j < a1 - 11:
         s = 0
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
+            for x in range(j, j + 10):
+                n1 = arr[n][x][0]
+                n2 = arr[n][x][1]
+                n3 = arr[n][x][2]
                 M = n1 + n2 + n3
                 s += M
         s = int(s // 100)
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+            for y in range(j, j + 10):
+                arr[n][y][0] = int(s // 50) * 50
+                arr[n][y][1] = int(s // 50) * 50
+                arr[n][y][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
-res.save('res.jpg')
+res.save('Cyber_old.res.jpg')
