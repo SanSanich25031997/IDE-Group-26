738b1e393e6723c0a2a5b46a36f871cd87e9c74f
Teelirium <74907594+Teelirium@users.noreply.github.com>
2021-11-24 15:54:10 +0500

    Тесты и их результаты

--- filter.py
+++ filter.py
@@ -1,50 +1,57 @@
 from PIL import Image
 import numpy as np
 
-imgIn = input("Введите имена исходного изображения")
-img = Image.open(imgIn)
-img_arr = np.array(img)
-height = len(img_arr)
-width = len(img_arr[1])
-cell_size = input("Введите размер мозаики (по умолч. 2)")
-try:
-    cell_size = int(cell_size)
-except ValueError:
-    cell_size = 2
-gray_scale_steps = input("Введите количество градаций серого (по умолч. 50)")
-try:
-    gray_scale_steps = int(gray_scale_steps)
-except ValueError:
-    gray_scale_steps = 50
 
-
-def get_gray_lvl_in_cell(img_arr, cell_size, i, j):
+def get_gray_color(img_arr, cell_size, i, j, gray_scale_steps):
     """
+    Находит цвет, в который надо покрасить ячейку мозаики
+    :param numpy.ndarray img_arr: Исходное изображение в виде матрицы numpy
+    :param int cell_size: Размер ячейки мозаики
+    :param int i: Координата y ячейки
+    :param int j: Координата x ячейки
+    :param int gray_scale_steps: Количество градаций серого
+    :return: Насыщенность найденного серого цвета
 
-    :param img_arr: Исходное изображение в виде матрицы numpy
-    :param cell_size: Размер мозаики
-    :param i: Начальная координата y
-    :param j:
-    :return:
+    >>> get_gray_color(np.array(Image.open("ahmed_epic.jpg")), 2, 0, 0, 50)
+    200
+    >>> get_gray_color(np.array(Image.open("ahmed_epic.jpg")), 5, 250, 250, 50)
+    100
     """
-    gray_level = 0
+    sat_level = 0
     for n in range(i, i + cell_size):
         for m in range(j, j + cell_size):
             red = img_arr[n][m][0]
             green = img_arr[n][m][1]
             blue = img_arr[n][m][2]
             rgb_sum = int(red) + int(green) + int(blue)
-            gray_level += rgb_sum / 3
-    return int(gray_level // (cell_size * cell_size))
+            sat_level += rgb_sum / 3
+    avg_sat = int(sat_level // (cell_size * cell_size))
+    return int(avg_sat // gray_scale_steps) * gray_scale_steps
 
+
+img_in = input("Введите имена исходного изображения\n")
+img = Image.open(img_in)
+img_arr = np.array(img)
+height = len(img_arr)
+width = len(img_arr[1])
+
+cell_size = input("Введите размер мозаики (по умолч. 2)\n")
+try:
+    cell_size = int(cell_size)
+except ValueError:
+    cell_size = 2
+
+gray_scale_steps = input("Введите количество градаций серого (по умолч. 50)\n")
+try:
+    gray_scale_steps = int(gray_scale_steps)
+except ValueError:
+    gray_scale_steps = 50
 
 i = 0
 while i < height - 1:
     j = 0
     while j < width - 1:
-        gray_level = get_gray_lvl_in_cell(img_arr, cell_size, i, j)
-        gray_color = int(gray_level // gray_scale_steps) * gray_scale_steps
-
+        gray_color = get_gray_color(img_arr, cell_size, i, j, gray_scale_steps)
         for n in range(i, i + cell_size):
             for m in range(j, j + cell_size):
                 img_arr[n][m][0] = gray_color
@@ -55,5 +62,5 @@ while i < height - 1:
     i = i + cell_size
 
 res = Image.fromarray(img_arr)
-res.save(f"res_{imgIn}")
+res.save(f"res_{img_in}")
 print("Результат готов!")
