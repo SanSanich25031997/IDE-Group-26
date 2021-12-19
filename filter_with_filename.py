8d4d40cddfa837303a513e08701e2ae67b0002f3
Ksenia <k.popova.21@mail.ru>
2021-12-19 18:09:06 +0500

    Task 2 (Попова Ксения)

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -16,14 +16,14 @@ def get_average_brightness(block, size, gradation_step):
 
 
 def main():
-    image_file = Image.open("scale1200.jpg")
+    image_file = Image.open("cat2.jpg")
     block_size = 10
     gradations_count = 50
     image = np.array(image_file)
     gradation_step = 255 // gradations_count
 
     res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
-    res.save("res_new.jpg")
+    res.save("res_cat2.jpg")
 
 
 if __name__ == '__main__':
